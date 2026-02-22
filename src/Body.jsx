f8dd7c6583373b90a05d9131b103241d37eef083
adit-ya15 <aditya262701@gmail.com>
2026-02-22 17:43:35 +0530

    Implement a check while you not authenticated you not able to access to any route

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from './constants/commonData'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from './redux/userSlice'
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector(store => store.user)
+    const fetchUser = async () => {
+        if (user) {
+            return;
+        }
+        try {
+            const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true })
+            console.log(res.data);
+            dispatch(addUser(res.data));
+        } catch (error) {
+            if (error.status === 401) {
+                navigate("/login")
+            }
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, [])
+
     return (
         <div>
             <Navbar />
